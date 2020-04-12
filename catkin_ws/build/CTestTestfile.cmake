353cfc03916dfa22940c4ea83326260f7a0c208c
jingshenglyu <jingshenglyu@gmail.com>
2020-04-12 12:49:25 +0200

    new build folder for chapter6

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,12 +5,9 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("HW_Chapter3")
 subdirs("learning_launch")
-subdirs("mrobot_teleop")
 subdirs("learning_tf")
 subdirs("turtle_control")
 subdirs("mbot_gazebo")
-subdirs("mrobot_gazebo")
-subdirs("mbot_description")
 subdirs("mbot_teleop")
-subdirs("mrobot_description")
