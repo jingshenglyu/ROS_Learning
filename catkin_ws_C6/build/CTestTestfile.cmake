fd2f0f7dd57c32ba6092d44e321ddb908886581b
jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2021-01-30 21:18:26 +0100

    Update the workspace of C6

---
+++ catkin_ws_C6/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/jingsheng/ROS_Learning/catkin_ws_C6/src
+# Build directory: /home/jingsheng/ROS_Learning/catkin_ws_C6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mbot_gazebo")
+subdirs("mbot_teleop")
---
+++ catkin_ws_C6/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jingsheng/ROS_Learning/catkin_ws_C6/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jingsheng/ROS_Learning/catkin_ws_C6/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/jingsheng/ROS_Learning/catkin_ws_C6/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/jingsheng/ROS_Learning/catkin_ws_C6/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/jingsheng/ROS_Learning/catkin_ws_C6/src:$ROS_PACKAGE_PATH"
