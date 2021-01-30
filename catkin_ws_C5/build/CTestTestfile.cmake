2b771aab1127ebf8bb2e79c1975bdef6d2c5dc2d
jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2021-01-30 21:18:07 +0100

    Update the workspace of C5

---
+++ catkin_ws_C5/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jingsheng/ROS_Learning/catkin_ws_C5/src
+# Build directory: /home/jingsheng/ROS_Learning/catkin_ws_C5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mbot_description")
---
+++ catkin_ws_C5/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jingsheng/ROS_Learning/catkin_ws_C5/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jingsheng/ROS_Learning/catkin_ws_C5/devel;/opt/ros/noetic'.split(';') if '/home/jingsheng/ROS_Learning/catkin_ws_C5/devel;/opt/ros/noetic' != '' else []
